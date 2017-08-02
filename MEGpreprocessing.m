fca640fc12d02a0352c765b458890ff659f1e05a
carohdez <hdez.carolina@gmail.com>
2017-08-02 13:46:51 +0200

    Saccades detection, velocity acceleration/approach
    Saccades detection, velocity acceleration/approach

--- MEGpreprocessing.m
+++ MEGpreprocessing.m
@@ -1,5 +1,4 @@
 function [] = MEGpreprocessing(subject, session, recording)
-
 addpath '/mnt/homes/home024/pmurphy/Toolboxes/fieldtrip-20160221'
 % %addpath '/Users/caro/Documents/UKE/Matlab/Toolboxes/fieldtrip-20170322'
 ft_defaults
@@ -182,6 +181,7 @@ tr_sacc = [];
 hz = 1200; % sampling frequency, hdr = ft_read_header(cfg.dataset);
 threshold = 30; % taken from Niklas's script
 acc_thresh = 2000; % taken from Niklas's script
+amp_thresh = 1.5; % our own saccadic amplitude threshold - meant to exclude all but most extreme microsaccades
 
 % For plotting
 max_tr = find(data.trialinfo(:,3)==max(data.trialinfo(:,3)));
@@ -193,10 +193,16 @@ for i=1:length(data.trial),
     [x, y, z] = eye_voltage2gaze(data.trial{i}, ranges, screen_x, screen_y, ch_mapping);
     ypos(i,1:length(y)) = y;
     sacc = check_saccade(x, y, xcenter, ycenter, ppd);
-%     if sacc == 0
-%         % Detect saccades with velocity acceleration approach
-%         sacc = check_saccade_vel_acc(x, y, hz, threshold, acc_thresh, ppd);
-%     end
+    try
+        if sacc == 0
+            % Detect saccades with velocity acceleration approach
+            sacc = check_saccade_vel_acc(x, y, hz, threshold, acc_thresh, amp_thresh, ppd);
+        end
+    catch
+        savepath = '/mnt/homes/home024/chernandez/meg_data/surprise/preprocessed/Data/';
+        namefile = strcat('ErrorTrial_',subject,'-',session,'_sacc_',recording,'.mat');
+        save([savepath,namefile],'i','x', 'y', 'hz', 'threshold', 'acc_thresh', 'amp_thresh', 'ppd','-v7.3'); 
+    end
     if sacc > 0
         tr_sacc = [tr_sacc, i];
     end
@@ -386,8 +392,8 @@ savepath = '/mnt/homes/home024/chernandez/meg_data/surprise/preprocessed/Plots/'
 namefile = strcat(subject,'-',session,'_Surprise_Preprocessed_Plot_',recording);
 
 
-% this is to verify the sacc new approach (to save only info trials
-% rejected)
+% % % this is to verify the sacc new approach (to save only info trials
+% % % rejected)
 % % %save data file
 % % savepath = '/mnt/homes/home024/chernandez/meg_data/surprise/preprocessed/Data/';
 % % namefile = strcat(subject,'-',session,'_Surprise_Preprocessed_sacc_',recording,'.mat');
@@ -508,7 +514,7 @@ function sacc = check_saccade(xgaze, ygaze, xcenter, ycenter, ppd)
     end
 end
 
-function [sacc, sacc_times] = check_saccade_vel_acc(xgaze, ygaze, Hz, threshold, acc_thresh, ppd)
+function [sacc, sacc_times] = check_saccade_vel_acc(xgaze, ygaze, Hz, threshold, acc_thresh, amp_thresh, ppd)
     
     sacc = 0;  % initializing binary output flag that indicates whether this trial contains a saccade
     sacc_times = [];
@@ -522,12 +528,34 @@ function [sacc, sacc_times] = check_saccade_vel_acc(xgaze, ygaze, Hz, threshold,
     borders = diff(saccades);   % start points of candidate saccades will be 1, end points will be -1, all others 0
     if saccades(1)>threshold, borders = [1 borders]; else borders = [0 borders]; end  % in case first sample violates threshold
     if saccades(end)>threshold, borders(end+1) = -1; end  % in case last sample violates threshold
-        
+    
+    window_size = 3;
+    win = ones(1,window_size)/double(window_size);
+    x = conv(x, win, 'same'); y = conv(y, win, 'same');   % lightly smoothing gaze time series before applying amplitude threshold
+    
     starts = find(borders==1); ends = find(borders==-1)-1;  % getting all start and end points of candidate saccades
-    for i = 1:length(starts)  % looping through all candidate saccades and only accepting if they also violate acceleration threshold
-        if ~isempty(find(acceleration(starts(i):ends(i))>acc_thresh))
-            sacc = 1;
-            sacc_times(end+1,1:2) = [starts(i) ends(i)];
+    if length(starts)>length(ends), starts(end) = []; end   % if last saccade occurs right at trial end (and so doesn't have corresponding ends), discard
+    for i = 1:length(starts)  % looping through all candidate saccades and only accepting if they also violate acceleration/amplitude thresholds
+        if ~isempty(find(acceleration(starts(i):ends(i))>acc_thresh))  % applying acceleration threshold
+            % applying amplitude threshold based on average gaze position over 3 samples before vs after saccade
+            if i>1 && starts(i)-3>0  % a few exceptions to try to make sure preceding 3 samples are useable
+                t1 = max([starts(i)-3,ends(i-1)]):starts(i)-1;
+            elseif starts(i)<=2
+                t1 = 1;
+            else t1 = starts(i)-1;
+            end
+            if i<length(starts) && ends(i)+3<=length(x)  % a few exceptions to try to make sure following 3 samples are useable
+                t2 = ends(i)+1:min([ends(i)+3,starts(i+1)]);
+            elseif ends(i)>=length(x)-1
+                t2 = length(x);
+            else t2 = ends(i)+1;
+            end
+            p1 = [mean(x(t1)) mean(y(t1))];  % x,y coords of pre-saccade gaze position (in d.v.a., always positive)
+            p2 = [mean(x(t2)) mean(y(t2))];  % x,y coords of post-saccade gaze position (in d.v.a., always positive)
+            if ((p1(1)-p2(1)).^2 + (p1(2)-p2(2)).^2).^.5 > amp_thresh;  % applying amplitude threshold
+                sacc = 1;
+                sacc_times(end+1,1:2) = [starts(i) ends(i)];
+            end
         end
     end
         
